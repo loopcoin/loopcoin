16cd5cfa5b77259990d98428455eb93262f36c6d
Jack Daines <dainesj@gmail.com>
2014-09-24 10:47:47 -0700

    added checkpoints

--- src/loopcoin.cpp
+++ src/loopcoin.cpp
@@ -2628,29 +2628,14 @@ int CLoopcoinHooks::LockinHeight()
 {
     if (fTestNet)
         return 0;
-//no lockin
-    return 0;
+    return 512;
 }
 
 bool CLoopcoinHooks::Lockin(int nHeight, uint256 hash)
 {
-//no lockin
-return true;
-
     if (!fTestNet)
-        if ((nHeight == 2016 && hash != uint256("0x0000000000660bad0d9fbde55ba7ee14ddf766ed5f527e3fbca523ac11460b92")) ||
-                (nHeight ==   4032 && hash != uint256("0x0000000000493b5696ad482deb79da835fe2385304b841beef1938655ddbc411")) ||
-                (nHeight ==   6048 && hash != uint256("0x000000000027939a2e1d8bb63f36c47da858e56d570f143e67e85068943470c9")) ||
-                (nHeight ==   8064 && hash != uint256("0x000000000003a01f708da7396e54d081701ea406ed163e519589717d8b7c95a5")) ||
-                (nHeight ==  10080 && hash != uint256("0x00000000000fed3899f818b2228b4f01b9a0a7eeee907abd172852df71c64b06")) ||
-                (nHeight ==  12096 && hash != uint256("0x0000000000006c06988ff361f124314f9f4bb45b6997d90a7ee4cedf434c670f")) ||
-                (nHeight ==  14112 && hash != uint256("0x00000000000045d95e0588c47c17d593c7b5cb4fb1e56213d1b3843c1773df2b")) ||
-                (nHeight ==  16128 && hash != uint256("0x000000000001d9964f9483f9096cf9d6c6c2886ed1e5dec95ad2aeec3ce72fa9")) ||
-                (nHeight ==  18940 && hash != uint256("0x00000000000087f7fc0c8085217503ba86f796fa4984f7e5a08b6c4c12906c05")) ||
-                (nHeight ==  30240 && hash != uint256("0xe1c8c862ff342358384d4c22fa6ea5f669f3e1cdcf34111f8017371c3c0be1da")) ||
-                (nHeight ==  57000 && hash != uint256("0xaa3ec60168a0200799e362e2b572ee01f3c3852030d07d036e0aa884ec61f203")) ||
-                (nHeight == 112896 && hash != uint256("0x73f880e78a04dd6a31efc8abf7ca5db4e262c4ae130d559730d6ccb8808095bf")) ||
-                (nHeight == 182000 && hash != uint256("0xd47b4a8fd282f635d66ce34ebbeb26ffd64c35b41f286646598abfd813cba6d9")))
+        if ((nHeight == 1 && hash != uint256("0x0000000059fa4eccdd2628c61e2f19618ba701898a0d33dfb79ee5d590b7ee87")) ||
+                (nHeight == 512 && hash != uint256("0x00000000ead9867310f287a47b08d61099a5dc4d985c9b84176b8318d71c9422")))
             return false;
     return true;
 }
