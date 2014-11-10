a0af35ecec98093f62d035bf85b98d0d3730aad7
root <root@fedora-jakx.localdomain>
2014-11-09 18:50:45 -0800

    fix string error

--- src/loopcoin.cpp
+++ src/loopcoin.cpp
@@ -2598,7 +2598,7 @@ bool GenesisBlock(CBlock& block, int extra)
     block.nTime    = 1303000001;
     block.nBits    = 0x1c007fff;
     block.nNonce   = 0xa21ea192U;
-    const char* pszTimestamp = "Carpe Diem - Horace;
+    const char* pszTimestamp = "Carpe Diem - Horace";
     CTransaction txNew;
     txNew.vin.resize(1);
     txNew.vout.resize(1);
